95f4985704224e05cf1ad7f3624bf60f85d63ba7
Samin Yeasar Abir <63140759+Y3454R@users.noreply.github.com>
2021-12-09 15:52:34 +0600

    Update creating_multiple_divs.php

--- creating_multiple_divs.php
+++ creating_multiple_divs.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
 <html>
+<head>
+<style>
+div {
+  border: 1px solid black;
+  //padding: 25px 50px 75px;
+  background-color: lightblue;
+  text-align: center;
+}
+p {
+  padding: 25px;
+}
+</style>
+</head>
 <body>
-    <?php for($i = 0; $i < 5; $i++) { ?>
-        <div>
-            <h3> <?php echo "$i" ?> </h3>
-        </div>
-    <?php } ?>
+
+<?php for($i = 0; $i < 5; ++$i) { ?>
+	<div>
+    	<p> <?php echo "This is div no. $i <br>" ?> </p>
+    </div>
+<?php } ?>
+
 </body>
 </html>
