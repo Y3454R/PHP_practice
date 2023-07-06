32024aebde3a012110e2c27af5e28e6055383b2b
Yeasar <yeasar1507099@stud.kuet.ac.bd>
2023-07-06 19:49:28 +0600

    trait

---
+++ PHP_OOP/trait.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+trait message1 {
+	public function msg() {
+    	echo "I am learning trait<br>";
+    }
+    public function hello() {
+    	echo "Hello world!<br>";
+    }
+}
+
+trait message2 {
+	public function msg2() {
+    	echo "Arekta Trait!<br>";
+    }
+}
+
+class Welcome {
+  	use message1, message2;
+}
+
+class Test {
+	use message1;
+    public function msg() {
+    	echo "Dekhi to ki hoy!<br>";
+    }
+}
+
+$obj = new Welcome();
+$obj->msg();
+$obj->msg2();
+$obj2 = new Test();
+$obj2->msg();
+$obj2->hello();
+?>
+ 
+</body>
+</html>
