247b3b6752a612c0ae97075733eff5384d16d83f
Yeasar <yeasar1507099@stud.kuet.ac.bd>
2023-07-07 03:36:36 +0600

    reformat

--- PHP_OOP/AcessModifierFunc.php
+++ PHP_OOP/AcessModifierFunc.php
@@ -3,32 +3,42 @@
 <body>
 
 <?php
-class Fruit {
-  public $name;
-  protected $color;
-  private $weight;
- 
-  function set_name($n) {  // a public function (default)
-    $this->name = $n;
-  }
-  protected function set_color($n) { // a protected function
-    $this->color = $n;
-  }
-  private function set_weight($n) { // a private function
-    $this->weight = $n;
-  }
-  
-  function acess_set_color($color) {
-  	$this->set_color($color);
-  }
-  
-  function acess_set_weight($w) {
-  	$this->set_weight($w);
-  }
-  
-  function show() {
-  	echo "name: ".$this->name." color: ".$this->color." weight: ".$this->weight."\n";
-  }
+
+class Fruit
+{
+    public $name;
+    protected $color;
+    private $weight;
+
+    function set_name($n)
+    {  // a public function (default)
+        $this->name = $n;
+    }
+
+    protected function set_color($n)
+    { // a protected function
+        $this->color = $n;
+    }
+
+    private function set_weight($n)
+    { // a private function
+        $this->weight = $n;
+    }
+
+    function acess_set_color($color)
+    {
+        $this->set_color($color);
+    }
+
+    function acess_set_weight($w)
+    {
+        $this->set_weight($w);
+    }
+
+    function show()
+    {
+        echo "name: " . $this->name . " color: " . $this->color . " weight: " . $this->weight . "\n";
+    }
 }
 
 $mango = new Fruit();
@@ -37,7 +47,8 @@ $mango->acess_set_color('Green');
 $mango->acess_set_weight('300 gm');
 
 $mango->show();
+
 ?>
- 
+
 </body>
 </html>
--- PHP_OOP/trait.php
+++ PHP_OOP/trait.php
@@ -3,29 +3,38 @@
 <body>
 
 <?php
-trait message1 {
-	public function msg() {
-    	echo "I am learning trait<br>";
+trait message1
+{
+    public function msg()
+    {
+        echo "I am learning trait<br>";
     }
-    public function hello() {
-    	echo "Hello world!<br>";
-    }
-}
 
-trait message2 {
-	public function msg2() {
-    	echo "Arekta Trait!<br>";
+    public function hello()
+    {
+        echo "Hello world!<br>";
     }
 }
 
-class Welcome {
-  	use message1, message2;
+trait message2
+{
+    public function msg2()
+    {
+        echo "Arekta Trait!<br>";
+    }
 }
 
-class Test {
-	use message1;
-    public function msg() {
-    	echo "Dekhi to ki hoy!<br>";
+class Welcome
+{
+    use message1, message2;
+}
+
+class Test
+{
+    use message1;
+    public function msg()
+    {
+        echo "Dekhi to ki hoy!<br>";
     }
 }
 
@@ -35,7 +44,8 @@ $obj->msg2();
 $obj2 = new Test();
 $obj2->msg();
 $obj2->hello();
+
 ?>
- 
+
 </body>
 </html>
