742b165c1bf1307eaa6bdacbb7e3344f4e1402ca
Yeasar <yeasar1507099@stud.kuet.ac.bd>
2023-07-06 18:05:35 +0600

    revise

---
+++ PHP_OOP/AcessModifierFunc.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Fruit {
+  public $name;
+  public $color;
+  public $weight;
+ 
+  function set_name($n) {  // a public function (default)
+    $this->name = $n;
+  }
+  protected function set_color($n) { // a protected function
+    $this->color = $n;
+  }
+  private function set_weight($n) { // a private function
+    $this->weight = $n;
+  }
+  
+  function acess_set_color($color) {
+  	$this->set_color($color);
+  }
+  
+  function acess_set_weight($w) {
+  	$this->set_weight($w);
+  }
+  
+  function show() {
+  	echo "name: ".$this->name." color: ".$this->color." weight: ".$this->weight."\n";
+  }
+}
+
+$mango = new Fruit();
+$mango->set_name('Mango');
+$mango->acess_set_color('Green');
+$mango->acess_set_weight('300 gm');
+
+$mango->show();
+?>
+ 
+</body>
+</html>
